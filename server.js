894f60ef65fb707deea74857f2efdf38e0b933e9
GitVCL <chat.vasconcelos@gmail.com>
2025-07-09 17:26:26 -0300

    BACK END 1.1

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ import dotenv from 'dotenv';
 
 import authRoutes from './src/routes/authRoutes.js';
 import comandaRoutes from './src/routes/comandaRoutes.js';
-import quartosRoutes from './src/routes/quartosRoutes.js';
 import finalizadosRoutes from './src/routes/finalizadosRoutes.js';
 import cardapioRoutes from './src/routes/cardapioRoutes.js';
 import relatorioRoutes from './src/routes/relatorioRoutes.js';
@@ -20,7 +19,6 @@ app.use(express.json());
 // Rotas
 app.use('/api/auth', authRoutes);
 app.use('/api/comandas', comandaRoutes);
-app.use('/api/quartos', quartosRoutes);
 app.use('/api/finalizados', finalizadosRoutes);
 app.use('/api/produtos', cardapioRoutes);
 app.use('/api/relatorios', relatorioRoutes);
